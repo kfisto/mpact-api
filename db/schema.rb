790da13502a5e0fdfba6916285154f45ef5bbae6
Dan LaMee <dan.lamee@gmail.com>
2015-11-11 19:38:10 -0500

    add include field to entries table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151014173843) do
+ActiveRecord::Schema.define(version: 20151112003344) do
 
   create_table "entries", force: true do |t|
     t.string  "guideKey"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151014173843) do
     t.integer "entrytype"
     t.string  "location"
     t.text    "bio",       limit: 2048
+    t.boolean "include",                default: true
   end
 
   create_table "guides", force: true do |t|
